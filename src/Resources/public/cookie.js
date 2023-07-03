cca31ff9f1cdc84ae39b2652e14ca68fd9a88cad
Klaus <support@kikwik.it>
2023-07-03 17:27:06 +0200

    reload page after consent

--- src/Resources/public/cookie.js
+++ src/Resources/public/cookie.js
@@ -2,17 +2,31 @@ document.addEventListener('click', function (event) {
 
     if (event.target.matches('.js-kwc-btn-accept') || event.target.matches('.js-kwc-btn-deny')) {
         event.preventDefault();
+        // make a POST request to consent url
         const url = event.target.href;
         fetch(url, {
             method: 'POST'
         }).then(function (response){
             if(response.ok)
             {
+                // close the banner
                 const banner = document.querySelector('.kwc-banner');
                 if(banner)
                 {
                     banner.style.display = 'none';
                 }
+
+                // reload the page
+                fetch(window.location.href)
+                    .then(function (response){
+                        // The call was successful!
+                        return response.text();
+                    }).then(function (html) {
+                    // This is the HTML from our response as a text string
+                    document.open('text/html');
+                    document.write(html);
+                    document.close();
+                });
             }
         })
     }
